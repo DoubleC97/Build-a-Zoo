82bb7863cd33bb7c393e3f0b2a446bf43c5b38fb
LAPTOP-GNIPG2O5\aaron <1502706@uad.ac.uk>
2017-11-15 14:19:02 +0000

    testing commit

---
+++ Assets/Aaron_s Prototypes (1)/Floater.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floater : MonoBehaviour
+{
+    public float waterLevel, floatHeight;
+    public Vector3 buoyancyCentreOffset;
+    public float bounceDamp;
+    Rigidbody rb;
+
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+            Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 actionPoint = transform.position + transform.TransformDirection(buoyancyCentreOffset);
+        float forceFactor = 1f - ((actionPoint.y - waterLevel) / floatHeight);
+
+        if (forceFactor > 0f)
+        {
+            Vector3 uplift = -Physics.gravity * (forceFactor - rb.velocity.y * bounceDamp);
+            rb.AddForceAtPosition(uplift, actionPoint);
+        }
+    }
+}
